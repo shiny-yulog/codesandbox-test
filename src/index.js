ce74061046d9f1d257cec5e6fdcba57ab4918e5d
shiny <114799784+shiny-yulog@users.noreply.github.com>
2022-10-06 06:26:04 +0900

    section5-35

--- src/index.js
+++ src/index.js
@@ -16,11 +16,30 @@ const onClickAdd = () => {
   const p = document.createElement("p");
   p.innerText = inputText;
 
+  //button(完了)タグを生成
+  const compButton = document.createElement("button");
+  compButton.innerText = "完了";
+  compButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  //button(削除)タグを生成
+  const delButton = document.createElement("button");
+  delButton.innerText = "削除";
+  delButton.addEventListener("click", () => {
+    //押された削除ボタンの親タグliを削除
+    const delChild = delButton.parentNode;
+    const delTarget = delChild.parentNode;
+    document.querySelector(".uncomplete-list").removeChild(delTarget);
+  });
+
   // liタグの子要素に各要素を設定
   li.appendChild(div);
 
   // divタグの子要素にp要素を設定
   div.appendChild(p);
+  div.appendChild(compButton);
+  div.appendChild(delButton);
 
   console.log(li);
 
